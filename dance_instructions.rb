593ad41b7d92019eff4902d5d60576bfb2880d67
tammy0508 <tamara.dandreamatteo@gmail.com>
2018-07-09 20:15:42 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,44 @@
 
+
+#lower body
+def starting_stance
+  puts "Assume proper stance:"
+  puts "Plant legs far apart,bend knees slightly and keep posture loose."
+end 
+
+#base footwork
+def base_footwork
+puts "Lift right foot"
+puts "Return right foot"
+end
+
+def skip_step(step)
+  puts "Lower"
+  puts "Bounce"
+  puts "Briefly"
+end
+
+#upper body 
+def bob_the_reins 
+  puts "Hold your arms out in front of you, straight and at chest level."
+  puts "Cross your right wrist over your left and hold them together."
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song."
+  puts "Do"
+end 
+
+def lasso
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Lift right arm up and point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso."
+  puts "Do"
+end
+  
+def bust_a_move
+  puts "Bust a move."
+  puts "Be cool and be snazzy."
+end
+  
+  
+  
+  
 # Code your methods below
